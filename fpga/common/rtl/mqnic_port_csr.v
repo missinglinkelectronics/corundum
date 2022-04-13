4b20ce6135a9fd459b6bf5b7daf1a7bf0cc0816f
Andreas Braun <andreas.braun@missinglinkelectronics.com>
2022-04-13 21:20:43 +0200

    fpga/common: Add Port CSR module
    Signed-off-by: Andreas Braun <andreas.braun@missinglinkelectronics.com>

---
+++ fpga/common/rtl/mqnic_port_csr.v
@@ -0,0 +1,122 @@
+/*
+
+Copyright 2022, MissingLinkElectronics Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY MISSINGLINKELECTRONICS INC. ''AS
+IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MISSINGLINKELECTRONICS INC. OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of MissingLinkElectronics Inc.
+
+*/
+
+// Language: Verilog 2001
+
+`resetall
+`timescale 1ns / 1ps
+`default_nettype none
+
+/*
+ * Port control and status registers
+ */
+module mqnic_port_csr #
+(
+    // Width of control register interface address in bits
+    parameter REG_ADDR_WIDTH = 16,
+    // Width of control register interface data in bits
+    parameter REG_DATA_WIDTH = 32,
+    // Width of control register interface strb
+    parameter REG_STRB_WIDTH = (REG_DATA_WIDTH/8),
+    // Register block base address
+    parameter RB_BASE_ADDR = 0,
+    // Register block next pointer
+    parameter RB_NEXT_PTR = 0
+)
+(
+    input  wire                          clk,
+    input  wire                          rst,
+
+    /*
+     * Control register interface
+     */
+    input  wire [REG_ADDR_WIDTH-1:0]     ctrl_reg_wr_addr,
+    input  wire [REG_DATA_WIDTH-1:0]     ctrl_reg_wr_data,
+    input  wire [REG_STRB_WIDTH-1:0]     ctrl_reg_wr_strb,
+    input  wire                          ctrl_reg_wr_en,
+    output wire                          ctrl_reg_wr_wait,
+    output wire                          ctrl_reg_wr_ack,
+    input  wire [REG_ADDR_WIDTH-1:0]     ctrl_reg_rd_addr,
+    input  wire                          ctrl_reg_rd_en,
+    output wire [REG_DATA_WIDTH-1:0]     ctrl_reg_rd_data,
+    output wire                          ctrl_reg_rd_wait,
+    output wire                          ctrl_reg_rd_ack
+);
+
+localparam RBB = RB_BASE_ADDR & {REG_ADDR_WIDTH{1'b1}};
+
+// control registers
+reg ctrl_reg_wr_ack_reg = 1'b0;
+reg [REG_DATA_WIDTH-1:0] ctrl_reg_rd_data_reg = 0;
+reg ctrl_reg_rd_ack_reg = 1'b0;
+
+assign ctrl_reg_wr_wait = 1'b0;
+assign ctrl_reg_wr_ack = ctrl_reg_wr_ack_reg;
+assign ctrl_reg_rd_data = ctrl_reg_rd_data_reg;
+assign ctrl_reg_rd_wait = 1'b0;
+assign ctrl_reg_rd_ack = ctrl_reg_rd_ack_reg;
+
+wire [REG_ADDR_WIDTH-1:0] ctrl_reg_rd_addr_mod;
+assign ctrl_reg_rd_addr_mod = {ctrl_reg_rd_addr >> 2, 2'b00};
+
+always @(posedge clk) begin
+    ctrl_reg_wr_ack_reg <= 1'b0;
+    ctrl_reg_rd_data_reg <= 0;
+    ctrl_reg_rd_ack_reg <= 1'b0;
+
+    if (ctrl_reg_wr_en && !ctrl_reg_wr_ack_reg) begin
+        // write operation
+        ctrl_reg_wr_ack_reg <= 1'b1;
+    end
+
+    if (ctrl_reg_rd_en && !ctrl_reg_rd_ack_reg) begin
+        // read operation
+        ctrl_reg_rd_ack_reg <= 1'b1;
+
+        case (ctrl_reg_rd_addr_mod)
+            // Port CSR
+            RBB+8'h00: ctrl_reg_rd_data_reg <= 32'h0000C070;  // Type
+            RBB+8'h04: ctrl_reg_rd_data_reg <= 32'h00000000;  // Version
+            RBB+8'h08: ctrl_reg_rd_data_reg <= RB_NEXT_PTR;   // Next header
+        endcase
+    end
+
+    if (rst) begin
+        ctrl_reg_wr_ack_reg <= 1'b0;
+        ctrl_reg_rd_ack_reg <= 1'b0;
+    end
+end
+
+endmodule
+
+`resetall
